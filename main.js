15d796536957e49499b1efaf307a6c1791e26169
岩崎 剛 <iwasakitakeshi@MacBook-Air.local>
2019-02-24 23:18:54 +0900

    canvas の実験

--- main.js
+++ main.js
@@ -78,7 +78,22 @@ Game.ctrl = function () {
 
 // 描画
 Game.draw = function () {
-    document.getElementById("test").textContent = Game.str;
+    //    document.getElementById("test").textContent = Game.str;
+
+    var ctx = document.getElementById("cv").getContext("2d");
+
+    var txt = "JavaScript"; //描画する文字
+
+    ctx.font = "italic 40px Arial"; //フォントにArial,40px,斜体を指定
+    ctx.fillStyle = "green"; //塗り潰し色を緑に
+
+    ctx.fillText(txt, 10, 50);      //テキストを塗り潰しで描画
+    ctx.fillText(txt, 10, 100, 100); //テキストの最大幅を100pxに指定
+
+    ctx.strokeStyle = "blue"; //輪郭線の色を青に
+
+    ctx.strokeText(txt, 10, 150);     //テキスト輪郭を描画
+    ctx.strokeText(txt, 10, 200, 100); //テキストの最大幅を100pxに指定
 };
 
 // 実行
